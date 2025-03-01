759f056e300fd9129e40a81c0ec89b409e6d6d94
meghadasgupta793 <meghadasgupta793@gmail.com>
2025-03-01 20:54:17 +0530

    Server ModifyURL

--- src/secrect.js
+++ src/secrect.js
@@ -1,9 +1,9 @@
 // secrect.js
 const config = {
-  url: 'http://localhost:3005'  || 'https://pro1.smart-punch.com',
+  url:'https://pro1.smart-punch.com',
   MODEL_URL: '/models',
-  ImgUrl: import.meta.env.VITE_EMPLOYEE_IMG_URL || 'http://localhost:3005/images/Employee',
-  VisitorImgUrl: import.meta.env.VITE_VISITOR_IMG_URL || 'http://localhost:3005/images/visitor',
+  ImgUrl:'http://localhost:3005/images/Employee',
+  VisitorImgUrl:'http://localhost:3005/images/visitor',
   CompanyName:  'NM EnterPrise',
   CompanyAddress:'6/15, Aalay, Opp. Bus Stop, Vastrapur, Ahmedabad - 380 015. Gujarat, India.',
   AdminDashbordRefreshTime: '60000',
