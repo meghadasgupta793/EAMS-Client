ba0588ecc981cc5dc3b77fa33363ee587bae8b6d
meghadasgupta793 <meghadasgupta793@gmail.com>
2025-03-01 20:36:43 +0530

    Configured URL rewrite using environment variable

--- src/secrect.js
+++ src/secrect.js
@@ -5,7 +5,7 @@ const config = {
   ImgUrl: import.meta.env.VITE_EMPLOYEE_IMG_URL || 'http://localhost:3005/images/Employee',
   VisitorImgUrl: import.meta.env.VITE_VISITOR_IMG_URL || 'http://localhost:3005/images/visitor',
   CompanyName: import.meta.env.VITE_COMPANY_NAME || 'NM EnterPrise',
-  CompanyAddress: import.meta.env.VITE_COMPANY_ADDRESS || '6/15, Aalay, Opp. Bus Stop, Vastrapur, Ahmedabad - 380 015. Gujarat, India.',
+  CompanyAddress:'6/15, Aalay, Opp. Bus Stop, Vastrapur, Ahmedabad - 380 015. Gujarat, India.',
   AdminDashbordRefreshTime: '60000',
 };
 
