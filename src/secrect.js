80857bdc5c2d9b89b61b4e0984ce81e6961478d8
meghadasgupta793 <meghadasgupta793@gmail.com>
2025-03-03 01:45:08 +0530

    Token Impliment and Bug Fixing-2

--- src/secrect.js
+++ src/secrect.js
@@ -1,12 +1,15 @@
 // secrect.js
 const config = {
-  url:'http://localhost:3005',
+ // url:'http://localhost:3005',  
+ // ImgUrl:'http://localhost:3005/images/Employee',
+ // VisitorImgUrl:'http://localhost:3005/images/visitor',
   MODEL_URL: '/models',
-  ImgUrl:'http://localhost:3005/images/Employee',
-  VisitorImgUrl:'http://localhost:3005/images/visitor',
   CompanyName:  'NM Solution',
   CompanyAddress:'6/15, Aalay, Opp. Bus Stop, Vastrapur, Ahmedabad - 380 015. Gujarat, India.',
   AdminDashbordRefreshTime: '60000',
+  url:'https://pro1.smart-punch.com',
+  ImgUrl:'https://pro1.smart-punch.com/images/Employee',
+  VisitorImgUrl:'https://pro1.smart-punch.com/images/visitor',
 };
 
 export default config;
