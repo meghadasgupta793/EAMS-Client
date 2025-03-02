44ab9ae5dac7b87b8da10813f3470b97df3da6af
meghadasgupta793 <meghadasgupta793@gmail.com>
2025-03-02 18:29:54 +0530

    SecretJS BAckEnd URL

--- src/secrect.js
+++ src/secrect.js
@@ -1,9 +1,9 @@
 // secrect.js
 const config = {
-  url:'http://localhost:3005',
+  url:'https://pro1.smart-punch.com/',
   MODEL_URL: '/models',
-  ImgUrl:'http://localhost:3005/images/Employee',
-  VisitorImgUrl:'http://localhost:3005/images/visitor',
+  ImgUrl:'https://pro1.smart-punch.com/images/Employee',
+  VisitorImgUrl:'https://pro1.smart-punch.com/images/visitor',
   CompanyName:  'NM EnterPrise',
   CompanyAddress:'6/15, Aalay, Opp. Bus Stop, Vastrapur, Ahmedabad - 380 015. Gujarat, India.',
   AdminDashbordRefreshTime: '60000',
